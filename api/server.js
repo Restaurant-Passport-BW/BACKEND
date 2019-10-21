bcf5bc628c88a5c3e19bc9602dce916d08c1b3b1
shanreed <srtech5set@gmail.com>
2019-10-21 15:13:08 -0700

    added database and users table

--- api/server.js
+++ api/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
-// const authRouter = require('../auth/authRouter.js');
-// const usersRouter = require('../users/usersRouter.js');
+const authRouter = require('../auth/authRouter.js');
+const usersRouter = require('../users/usersRouter.js');
 
 const server = express();
 
@@ -12,8 +12,8 @@ server.use(cors())
 server.use(express.json());
 
 
-// server.use('/rpp/auth', authRouter);
-// server.use('/rpp/users', usersRouter);
+server.use('/rpp/auth', authRouter);
+server.use('/rpp/users', usersRouter);
 
 server.get('/', (req, res) => {
     res.send("This Is The Restaurant Passport Server")
